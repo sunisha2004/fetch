135647f5d70a4bb92c3acb8f812dab129dabf7bb
sunisha <sunishams2004@gmail.com>
2024-09-10 04:49:30 +0530

    commit 5

--- js/details.js
+++ js/details.js
@@ -8,21 +8,26 @@ async function getDetails() {
     const res=await fetch(`https://dummyjson.com/products/${id}`);
     const data=await res.json()
     console.log(data);
+
+    const originalPrice = calculateOriginalPrice(data.price, data.discountPercentage);
+
+    let img=``
+    data.images.map((image)=>{
+        img+=`<img src=${image} alt="no image" onmouseover="changePic('${image}')"/>`
+    })
+    document.getElementById("images").innerHTML=img
     document.getElementById("container").innerHTML=`
-    <div id="subimg">
-    <img id="subimgs" src="${data.images[0]}" alt="">
-    <img id="subimgs" src="${data.images[1]}" alt="">
-    <img id="subimgs" src="${data.images[2]}" alt="">
-    </div>
     <div id="container1"> 
-    <div id="image">
+        <div id="image">
             <img id="img1" src="${data.thumbnail}" alt="">
         </div>
         <div id="productDetails">
             <div id="brand">${data.brand}</div>
             <div id="title">${data.title}</div>
-            <div id="price">Rs.${data.price}</div>
-            <div id="offer">${data.discountPercentage}%offer</div>
+            <div id="originalPrice">Rs.<del>${originalPrice.toFixed(2)}</del></div>
+           <div id="price">Rs.${data.price}</div>
+           
+            <div id="offer">${data.discountPercentage}% offer</div>
              <div id="rating">Rating : ${data.rating}</div>
 
         </div>
@@ -36,3 +41,12 @@ async function getDetails() {
     
 }
 getDetails()
+
+function changePic(image){
+    console.log(image);
+    document.getElementById("img1").src=image;
+    
+}
+function calculateOriginalPrice(discountedPrice, offerPercentage) {
+    return discountedPrice / (1 - (offerPercentage / 100));
+}
--- js/script.js
+++ js/script.js
@@ -38,4 +38,48 @@
 }
 getproducts()
 
+document.getElementById("search").addEventListener("keyup",async(e)=>{
+    console.log(e.target.value);
+
+    const res = await fetch("https://dummyjson.com/products");
+    if (res.status == 200){
+        const data = await res.json();
+        console.log(data);
+        products = data.products;
+        console.log(products);
+        str = ``;
+
+        filter=products.filter((item)=>item.title.toLowerCase().includes(e.target.value.toLowerCase()))
+        filter.map((product)=>{
+            
+            console.log(product);
+
+            str += `
+             <div class="cards">
+            <a href="../pages/detail.html?id=${product.id}">
+                <div class="image">
+                    <img src="${product.thumbnail}" alt="no image">
+                </div>
+                <div class="details">
+                    <h4 class="brand">${product.brand}</h4>
+                    <h4 class="name">${product.title}</h4>
+                    <h4 class="price">Rs.${product.price}</h4>
+                    <h5 class="offer">${product.discountPercentage}%off</h5>
+                    <br>
+                    <button>Cart</button>
+
+                </div>
+            </a>
+
+        </div>
+            `
+            
+        });
+        document.getElementById("products").innerHTML = str;
+        
+        
+        
+    }
+
+})
 
